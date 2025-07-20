076afd0ceee573b138d9c402bbf83579f1fe23c0
VozDuh <61590217+Voz-Duh@users.noreply.github.com>
2025-07-20 04:39:05 +0200

    Update mis_tables.h

--- include/mis_tables.h
+++ include/mis_tables.h
@@ -538,7 +538,7 @@ const unsigned char symbols_flags_trie[65279] = {
 
 //////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////
-// Copyright 2018 Ulf Adams
+// Copyright 2018 Ulf Adams. All rights reserved.
 // Boost Software License - Version 1.0 - August 17th, 2003
 //
 // Permission is hereby granted, free of charge, to any person or organization
