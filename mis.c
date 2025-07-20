0a1b5d4613e53918bd8118fe99012a53932a4717
VozDuh <61590217+Voz-Duh@users.noreply.github.com>
2025-07-20 04:38:26 +0200

    Update mis.c

--- mis.c
+++ mis.c
@@ -299,7 +299,7 @@ void mis_init(MISFallbackType _mis_fallback) {
 
 //////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////
-// Copyright 2018 Ulf Adams
+// Copyright 2018 Ulf Adams. All rights reserved.
 // Boost Software License - Version 1.0 - August 17th, 2003
 //
 // Permission is hereby granted, free of charge, to any person or organization
